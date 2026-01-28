5ab9e8a0d992d1d24099ef4f3a73ed6efe71eff7
Leopoldo Romano <leopoldoromano7@gmail.com>
2026-01-28 13:58:40 +0100

    fix

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -10,47 +10,19 @@
                         {{-- Titolo --}}
                         <div class="mb-3">
                             <label for="title" class="form-label">Titolo</label>
-                            <input 
-                                type="text" 
-                                name="title" 
-                                id="title" 
-                                class="form-control" 
-                                value="{{ old('title') }}" 
-                                required
-                            >
-                            @error('title')
-                                <div class="text-danger">{{ $message }}</div>
-                            @enderror
+                            <input type="text" name="title" id="title" class="form-control"  value="" required>
                         </div>
 
-                        {{-- Descrizione --}}
                         <div class="mb-3">
                             <label for="description" class="form-label">Descrizione</label>
-                            <textarea 
-                                name="description" 
-                                id="description" 
-                                rows="4" 
-                                class="form-control" 
-                                required
-                            >{{ old('description') }}</textarea>
-                            @error('description')
-                                <div class="text-danger">{{ $message }}</div>
-                            @enderror
+                            <textarea name="description"  id="description" rows="4" class="form-control" required ></textarea>
+                 
                         </div>
 
-                        {{-- Immagine --}}
                         <div class="mb-3">
                             <label for="img" class="form-label">Immagine (facoltativa)</label>
-                            <input 
-                                type="file" 
-                                name="img" 
-                                id="img" 
-                                class="form-control" 
-                                accept="image/*"
-                            >
-                            @error('img')
-                                <div class="text-danger">{{ $message }}</div>
-                            @enderror
+                            <input  type="file"  name="img" id="img" class="form-control" accept="image/*">
+                  
                         </div>
 
                         <button type="submit" class="btn btn-primary">Pubblica</button>
