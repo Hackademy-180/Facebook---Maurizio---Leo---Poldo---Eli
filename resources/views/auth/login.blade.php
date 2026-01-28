e9bca9d93f1c1777b17d71236361b64688cd20ce
Maurizio Catino <mauriziocatino96@gmail.com>
2026-01-28 11:31:50 +0100

    finita authentication

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,31 @@
+<x-layout>
+    <header class="container-fluid ">
+        <section class="row ">
+            <article class="col-12 ">
+                <h1 class=" text-center justify-content-center display-2 lead">Login</h1>
+            </article>
+        </section>
+    </header>
+
+
+    <main class="container-fluid">
+        <section class="row justify-content-center align-items-center">
+            <article class="col-12 col-md-8">
+                <form method="POST" action="{{route('login')}}">
+                    @csrf
+
+
+                        <label for="email" class="form-label"><strong>Email</strong></label>
+                        <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email">
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label"><strong>Password</strong></label>
+                        <input type="password" class="form-control" id="password" name="password">
+                    </div>    
+                    <button type="submit" class="btn btn-primary"><strong>Login</strong></button>
+                    <p class="mt-3">Non hai ancora  un account? Registrati da questo <a href="{{route('register')}}">link</a></p>
+                </form>
+            </article>
+        </section>
+    </main>
+</x-layout>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,39 +1,43 @@
 <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">F</a>
+    <a class="navbar-brand" href="#">Facebook</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <form class="d-flex" role="search">
-      <input class="form-control me-2" type="search" placeholder="Cerca su Facebook" aria-label="Search"/>
-    </form>
-  </div>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-      <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-      </li>
-    </ul>
-  </div>
-  <div>
-    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Cerca su Facebook" aria-label="Search"/>
+          <button class="btn btn-light" type="submit">Cerca</button>
+        </form>
+        @auth
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Benvenuto {{Auth::user()->name}}
+          </a>
+          <ul class="dropdown-menu">
+            <form action="{{route('logout')}}" method="POST">
+              @csrf
+              <button class="dropdown-item" type="submit">Logout</button>
+            </form>
+          </li>
+        </ul>
+      </li>  
+      @else            
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Il mio profilo
+          Utente
         </a>
         <ul class="dropdown-menu">
-          
-          <li><a class="dropdown-item" href="{{route('login')}}">Log-in</a></li>
           <li><a class="dropdown-item" href="{{route('register')}}">Registrati</a></li>
-          <li><a class="dropdown-item" href="">Modifica profilo</a></li>
-                      <li>
-              <form action="{{route('logout')}}" method="POST">
-                @csrf
-                <button class="dropdown-item" type="submit">Logout</button>
-              </form>
-            </li>
+          <li><a class="dropdown-item" href="{{route('login')}}">Login</a></li>
         </ul>
-      </li>
-    </ul>
-  </div>  
+      </li>  
+      @endauth
+    </div>
+  </div>
+</nav>
 </nav>
