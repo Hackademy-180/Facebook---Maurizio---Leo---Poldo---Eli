3036d0faef01fe36c02ca2bfacfcf8c5247c7833
leonardo lauria <laurialeonardo5@gmail.com>
2026-01-28 17:31:17 +0100

    risolto problema delete

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -78,6 +78,6 @@ class PostController extends Controller
     public function destroy(Post $post)
     {
         $post->delete();
-        return redirect("home");
+        return redirect(route("home"));
     }
 }
