cac88cdf0ef549c9fe29216d1c7295efac0a1a30
Maurizio Catino <mauriziocatino96@gmail.com>
2026-01-28 12:09:06 +0100

    finita navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,43 +1,58 @@
 <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Facebook</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+    <!-- SINISTRA: Facebook + Search -->
+    <div class="d-flex align-items-center">
+      <a class="navbar-brand me-3" href="{{ route('home') }}"><i class="fa-brands fa-facebook fa-2x"></i></a>
+
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search"
+               placeholder="Cerca su Facebook">
+        <button class="btn btn-light" type="submit">Cerca</button>
+      </form>
+    </div>
+
+    <button class="navbar-toggler" type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarSupportedContent">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Cerca su Facebook" aria-label="Search"/>
-          <button class="btn btn-light" type="submit">Cerca</button>
-        </form>
+
+    <!-- DESTRA: Utente -->
+    <div class="collapse navbar-collapse justify-content-end"
+         id="navbarSupportedContent">
+      <ul class="navbar-nav mb-2 mb-lg-0">
+
         @auth
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Benvenuto {{Auth::user()->name}}
+          <a class="nav-link dropdown-toggle" href="#"
+             data-bs-toggle="dropdown">
+            Benvenuto {{ Auth::user()->name }}
           </a>
           <ul class="dropdown-menu">
-            <form action="{{route('logout')}}" method="POST">
-              @csrf
-              <button class="dropdown-item" type="submit">Logout</button>
-            </form>
-          </li>
-        </ul>
-      </li>  
-      @else            
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Utente
-        </a>
-        <ul class="dropdown-menu">
-          <li><a class="dropdown-item" href="{{route('register')}}">Registrati</a></li>
-          <li><a class="dropdown-item" href="{{route('login')}}">Login</a></li>
-        </ul>
-      </li>  
-      @endauth
+            <li>
+              <form action="{{ route('logout') }}" method="POST">
+                @csrf
+                <button class="dropdown-item" type="submit">Logout</button>
+              </form>
+            </li>
+          </ul>
+        </li>
+        @else
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#"
+             data-bs-toggle="dropdown">
+            Utente
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="{{ route('register') }}">Registrati</a></li>
+            <li><a class="dropdown-item" href="{{ route('login') }}">Login</a></li>
+          </ul>
+        </li>
+        @endauth
+
+      </ul>
     </div>
+
   </div>
-</nav>
 </nav>
