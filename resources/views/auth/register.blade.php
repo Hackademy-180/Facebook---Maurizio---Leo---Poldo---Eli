966339ff868174d918cf82d0a5d6f39fe5e7227a
Maurizio Catino <mauriziocatino96@gmail.com>
2026-01-28 11:19:31 +0100

    iniziata authentication

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,30 @@
+<x-layout>
+    <main class="container">
+        <section class="row">
+            <div class="col-12">
+                <h1 class="text-center lead display-2">Form di registrazione</h1>
+                <form method="POST" action="{{route('register')}}">
+                   @csrf
+                   <div class="mb-3">
+                       <label for="name" class="form-label"><strong>Nome utente</strong></label>
+                       <input type="name" class="form-control" id="name" aria-describedby="emailHelp" name="name">
+                   </div>
+                   <div class="mb-3">
+                       <label for="email" class="form-label"><strong>Email</strong></label>
+                       <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email">
+                   </div>
+                   <div class="mb-3">
+                       <label for="password" class="form-label"><strong>Password</strong></label>
+                       <input type="password" class="form-control" id="password" name="password">
+                   </div>
+                   <div class="mb-3">
+                       <label for="password-confirmation" class="form-label"><strong>Conferma password</strong></label>
+                       <input type="password-confirmation" class="form-control" id="password-confirmation" name="password_confirmation">
+                   </div>                  
+                   <button type="submit" class="btn btn-primary">Registrati</button>
+               </form>
+            </div>
+        </section>
+    </main>
+    
+</x-layout> 
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,36 +1,39 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Facebook</a>
+    <a class="navbar-brand" href="#">F</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Post</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Il mio profilo
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Log-in</a></li>
-            <li><a class="dropdown-item" href="#">Registrati</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
+    <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Cerca su Facebook" aria-label="Search"/>
+    </form>
   </div>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+      </li>
+    </ul>
+  </div>
+  <div>
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Il mio profilo
+        </a>
+        <ul class="dropdown-menu">
+          
+          <li><a class="dropdown-item" href="{{route('login')}}">Log-in</a></li>
+          <li><a class="dropdown-item" href="{{route('register')}}">Registrati</a></li>
+          <li><a class="dropdown-item" href="">Modifica profilo</a></li>
+                      <li>
+              <form action="{{route('logout')}}" method="POST">
+                @csrf
+                <button class="dropdown-item" type="submit">Logout</button>
+              </form>
+            </li>
+        </ul>
+      </li>
+    </ul>
+  </div>  
 </nav>
