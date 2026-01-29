c9a0d6e8188a796dc27c52b167dc2fabde571bf0
Elisabetta Petruzzi Merli <elisabetta.petruzzimerli@gmail.com>
2026-01-29 16:21:42 +0100

    profilo in lavorazione

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Post;
+use App\Models\Comment;
+use App\Models\Profile;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
